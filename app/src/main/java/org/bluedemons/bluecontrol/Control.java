a47540808f6bd0656b78d91ebfd0b7966e9983fa
Sidhin S Thomas <sidhin.thomas@gmail.com>
2015-11-03 20:55:43 +0530

    Added Bluetooth read/write.
    Tested for fixed working with arduino, successfull transmission.

--- app/src/main/java/org/bluedemons/bluecontrol/Control.java
+++ app/src/main/java/org/bluedemons/bluecontrol/Control.java
@@ -16,6 +16,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.ParcelUuid;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,6 +25,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 
@@ -32,13 +36,16 @@ public class Control extends ActionBarActivity {
     private Button led ;
     private String btAddress;
 
-    final private UUID id = UUID.fromString("01001101-0000-1010-8000-00805F9B34FB");
+    private UUID id ;
 
-    private BluetoothAdapter Adapter = null;
-    private BluetoothDevice control = null;
-    private BluetoothSocket channel = null;
+    private BluetoothAdapter adapter = null;
+    private BluetoothDevice btDevice = null;
+    private BluetoothSocket btSocket = null;
 
+    private BtTransmission btTransmit;
 
+    private Boolean sendSuccess ;
+    private Boolean readSuccess ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,8 +53,12 @@ public class Control extends ActionBarActivity {
         led = (Button) findViewById(R.id.led);
         Intent Control = getIntent();
         btAddress = Control.getStringExtra(BTdeviceSelect.EXTRA_DEVICE_ADDRESS);
-        Adapter = BluetoothAdapter.getDefaultAdapter();
-        control =  Adapter.getRemoteDevice(btAddress);
+        adapter = BluetoothAdapter.getDefaultAdapter();
+        btDevice =  adapter.getRemoteDevice(btAddress);
+        ParcelUuid[] uuid = btDevice.getUuids();
+        id = UUID.fromString(uuid[0].toString());
+        BtConnectThread connect = new BtConnectThread();
+        connect.run();
     }
 
     @Override
@@ -65,14 +76,75 @@ public class Control extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
     public void led_onclick(View v){
-        Toast.makeText(getApplicationContext(),btAddress,Toast.LENGTH_LONG).show();
+        btTransmit.write("Hello\n".getBytes());
+    }
+    private void showMsg(String s){
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
 
     }
+    private void goBack(){
+        Intent intent = new Intent(Control.this,BTdeviceSelect.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+    private class BtConnectThread extends Thread {
+        public BtConnectThread(){
+            try{
+                btSocket = btDevice.createRfcommSocketToServiceRecord(id);
+                btTransmit = new BtTransmission();
+                btTransmit.inStream = btSocket.getInputStream();
+                btTransmit.outStream = btSocket.getOutputStream();
+                showMsg("Socket Created");
+
+            }catch (IOException e){
+                showMsg("Failed to communicate with the device please try again!");
+                goBack();
+            }
+        }
+        public void run(){
+            adapter.cancelDiscovery();
+            try {
+                btSocket.connect();
+                showMsg("Connected!");
+            }
+            catch (IOException e){
+                showMsg("Connection failed");
+                goBack();
+            }
+        }
+    }
+    private class BtTransmission extends Thread{
+
+        private InputStream inStream;
+        private OutputStream outStream;;
+
+
+        public void write(byte[] write){
+            try {
+                outStream.write(write);
+                sendSuccess = true;
+            }catch (IOException e){
+                showMsg("Sending failed");
+                sendSuccess = false;
+            }
+        }
+        public int read(){
+            try{
+                return inStream.read();
+            }catch (IOException e){
+                showMsg("Read fail");
+                readSuccess = false;
+                return -989;
+            }
+        }
+    }
+
 }
